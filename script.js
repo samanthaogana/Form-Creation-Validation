866f41d589de011ee996c0a135984165ec0c9b02
Samantha Ogana <samanthaogana@gmail.com>
2024-10-10 22:30:53 +0300

    Modify file

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('registration-form');
+    const feedbackDiv = document.getElementById('form-feedback');
+});
 
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+});
