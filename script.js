fe87061d5f49f220a191ee087d18a289f055bdb6
Samantha Ogana <samanthaogana@gmail.com>
2024-10-11 11:37:40 +0300

    Add code to script.js

--- script.js
+++ script.js
@@ -6,3 +6,37 @@ document.addEventListener('DOMContentLoaded', function() {
 form.addEventListener('submit', function(event) {
     event.preventDefault();
 });
+
+const usernameInput = document.getElementById('username').value.trim();
+const emailInput = document.getElementById('email').value.trim();
+const passowrdInput = document.getElementById('password').value.trim();
+
+let isValid = true;
+let messages = [];
+
+if (username.value.length < 3) {
+    isValid = false;
+    messages.push('Username must be at least 3 characters long');
+}
+
+if (!email.includes('@') || !email.includes('.')) {
+    isValid = false;
+    messages.push('Email must include both "@" and "." characters')
+}
+
+if(password.value.length < 8) {
+    isValid = false;
+    messages.push('Password must be at least 8 characters long');
+}
+
+feedbackDiv.style.display = 'block';
+
+if (isValid) {
+    feedbackDiv.textContent = 'Registration successful!';
+    feedbackDiv.style.color = '#28a745';
+}
+
+else {
+    feedbackDiv.innerHTML = messages.join('<br>');
+    feedbackDiv.style.color = '#dc3545';
+}
